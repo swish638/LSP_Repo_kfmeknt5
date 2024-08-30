3f417b89fe90719117ce26e893bd86b42ca60a06
adamm <adamm@LAPTOP-CFTJSL8K>
2024-08-30 16:21:54 -0400

    First commit of assignment 1.

---
+++ LSP_Fall_2024/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+*  Name: Chase Adams IV
+*/
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+
+	}
+
+}
