79e766409ef0aefad6af76e22b9c3f9a2255730d
lioan <lioan@DESKTOP-IO3TF4O>
2024-09-18 14:27:43 -0400

    trying to learn how to submit assignment 2

---
+++ LSP_Fall_2024_Keep_Trying/src/LSP_Fall_2024_Assignments/LSP_Fall_2024_Assignment2.java
@@ -0,0 +1,17 @@
+
+/**
+*  Name: Chase Adams IV 
+*/
+
+
+
+package LSP_Fall_2024_Assignments;
+
+public class LSP_Fall_2024_Assignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
